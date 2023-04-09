94bbaccd932cf907e7b91ae9484b2fb45ecb9b8c
silvijj <silvianiajoiesa@gmail.com>
2023-04-09 21:54:52 +0700

    implement pep 8, update variable name

--- modul.py
+++ modul.py
@@ -1,9 +1,10 @@
 """
-This module is built for support the self-service cashier of supermarket program ,consist of several functions:
+This module is built for support the self-service cashier of supermarket 
+program ,consist of several functions:
   add_item,
-  update item
-  delete item
-  reset item
+  update item,
+  delete item,
+  reset item,
   calculate transaction (all item)
 """
 
@@ -16,7 +17,7 @@ class Transaction:
   Attribute: name (string)
   """
   # dictionary to hold data items 
-  dct = {}
+  items = {}
 
   # dictionary of rule to get discount
   discount = {
@@ -25,57 +26,57 @@ class Transaction:
       200_000 : 5,
   }
 
-  def __init__(self, name):
+  def __init__(self, customer_name):
     """
     Constructs all necessary attributes for transaction object
-    Parameter: name represent customer name
+    Parameter: customer_name
     """
-    self.name = name
+    self.customer_name = customer_name
 
-  def add_item(self,item_name, quantity, price):
+  def add_item(self,name, quantity, price):
     """
     Function for add item
     Parameter: item name (string), quantity (int), price (int)
     """
-    self.dct.update({item_name:[quantity,price]})
+    self.items.update({name:[quantity,price]})
   
-  def update_item_name(self,item_name,new_item_name):
+  def update_item_name(self, name, new_name):
     """
     Function for update item name
     Parameter: item name (string), new item name (string)
     """
-    self.dct[new_item_name] = self.dct[item_name]
-    del self.dct[item_name]
+    self.items[new_name] = self.items[name]
+    del self.items[name]
 
-  def update_quantity(self,item_name,new_quantity):
+  def update_item_quantity(self, name, new_quantity):
     """
     Function for update item quantity
     Parameter: item name (string), new item quantity (string)
     """
-    self.dct[item_name].insert(0,new_quantity)
-    self.dct[item_name].pop(1)
+    self.items[name].insert(0,new_quantity)
+    self.items[name].pop(1)
 
-  def update_price(self,item_name,new_price):
+  def update_item_price(self, name, new_price):
     """
     Function for update item price
     Parameter: item name (string), new item price (string)
     """
-    self.dct[item_name].insert(1,new_price)
-    self.dct[item_name].pop(2)
+    self.items[name].insert(1,new_price)
+    self.items[name].pop(2)
     
-  def delete_item(self,item_name):
+  def delete_item(self, name):
     """
     Function for delete one item
     Parameter: item name that will be delete
     """
-    self.dct.pop(item_name)
+    self.items.pop(name)
 
   def reset_transaction(self):
     """
     Function for delete all item
     Parameter: none
     """
-    self.dct.clear()
+    self.items.clear()
 
   def check_order(self):
     """
@@ -84,29 +85,44 @@ class Transaction:
     """
     no = 0
     print_list = []
+    headers = [
+      'No',
+      'Nama Item',
+      'Jumlah Item',
+      'Harga/Item',
+      'Total Item'
+    ]
 
-    for k, v in self.dct.items():
+    for key, value in self.items.items():
       no += 1
-      quantity = v[0]
-      price = v[1]
+      quantity = value[0]
+      price = value[1]
       amount = quantity * price
-      add_list = [no,k,quantity,price,amount]
+      add_list = [
+          no, 
+          key, 
+          quantity, 
+          price, 
+          amount
+      ]
+
       print_list.append(add_list)
 
-    print(tabulate(print_list, headers=['No','Nama Item','Jumlah Item','Harga/Item','Total Item'], tablefmt="github"))
+    print(tabulate(print_list, headers = headers, tablefmt="github"))
 
   def total_price(self):
     """
-    Function for summary all item (total amount, total quantity, discount, payment total)
+    Function for summary all item (total amount, total quantity, discount, 
+    payment total)
     Parameter: none
     """
     total_amount = 0
     total_quantity = 0
     get_discount = 0
 
-    for value in self.dct:
-      amount = self.dct[value][0] * self.dct[value][1]
-      quantity = self.dct[value][0]
+    for value in self.items:
+      amount = self.items[value][0] * self.items[value][1]
+      quantity = self.items[value][0]
       total_amount = total_amount + amount
       total_quantity = total_quantity + quantity
     
@@ -127,8 +143,9 @@ class Transaction:
 
     print(tabulate(summary, tablefmt="plain"))
     print('')
-    print(f"Total belanja yang harus dibayar adalah Rp. {payment_total:,.2f}")
+    print(f"Total belanja yang harus dibayar {self.customer_name} adalah Rp. \
+    {payment_total:,.2f}")
 
   def __str__(self):
     """Function for get all items already entered"""
-    return f'Item yang dibeli {self.name} adalah: {self.dct}'
+    return f'Item yang dibeli {self.customer_name} adalah: {self.items}'
